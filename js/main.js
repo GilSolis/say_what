e023a04d5b6febca21dba1d626160caf9c6bfc20
Gil Solis <gilsolisuw@gmail.com>
2020-12-11 21:16:27 -0800

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+console.log("we here");
 
+
+document.getElementById('words').addEventListener('change', voice)
+
+function voice() {
+
+    const synth = window.speechSynthesis
+    let yourWords = document.getElementById('words').value
+    document.getElementById("sayWhat").innerText = yourWords
+    document.getElementById('words').value = ''
+
+
+    let wordsToSay = new SpeechSynthesisUtterance(yourWords)
+    synth.speak(wordsToSay)
+}
